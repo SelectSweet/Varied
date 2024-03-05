0f3a76daa91c808847116c7198094b65ac42d416
SelectSweet <>
2024-03-05 21:23:17 +1100

    Added Opendal & Removed Rclone Support for Image

--- src/Image.rs
+++ src/Image.rs
@@ -175,7 +175,7 @@ pub async fn ProcessImages(
             "Album": true     
         });
 
-        let ImageStorageURL = ImageStorageUrl + &PublicID + ".webp";
+        let ImageStorageURL = ImageStorageUrl + "/" + &images[0];
 
         let image = v_media::ActiveModel {
             id: ActiveValue::Set(ID),
@@ -196,6 +196,10 @@ pub async fn ProcessImages(
 
         let collection = add_to_collection(details, cookies).await;
 
+        PushImage(Paths[0].to_owned(), PublicID.to_owned(), images[0].to_owned(), oper).await;                
+    
+        std::fs::remove_dir_all(Process.to_owned() + "/" + &ImageBucket + "/" + &PublicID.to_owned()).unwrap();
+
         let result = json!({
             "Result": "Success",
             "Publicid": PublicID,
@@ -203,9 +207,7 @@ pub async fn ProcessImages(
         });
 
         return result;
-    }
-    
-     else if poster == true {
+    } else if addtocollection == true && addtoalbum == false {        
 
         FfmpegCommand::new()
             .input(image_path)
@@ -216,13 +218,63 @@ pub async fn ProcessImages(
             .unwrap()
             .iter()
             .expect("Image Not comverted");
-          
+        
         let properties = json!({
             "Poster": false,
             "Album": false,
             "Avatar": false
         });
 
+        let ImageStorageURL = ImageStorageUrl + "/" + &images[0];
+
+        let image = v_media::ActiveModel {
+            id: ActiveValue::Set(ID),
+            publicid: ActiveValue::Set(PublicID.to_owned()),
+            title: ActiveValue::Set(name.to_owned()),
+            mediatype: ActiveValue::Set("Image".to_string()),
+            uploaded_at: ActiveValue::Set(DateTime::new(now.date_naive(), now.time())),
+            username: ActiveValue::Set(username),
+            description: ActiveValue::Set(Some(description)),
+            chapters: ActiveValue::NotSet,
+            storagepathorurl: ActiveValue::Set(Some(vec![ImageStorageURL])),
+            poster_storagepathorurl: ActiveValue::NotSet,
+            properties: ActiveValue::Set(Some(properties)),
+            state: ActiveValue::Set(Media::MediaState::Published.to_string()),
+        };
+
+        let image: v_media::Model = image.insert(&connection).await.unwrap();
+
+        let collection = add_to_collection(details, cookies).await;
+
+        PushImage(Paths[0].to_owned(), PublicID.to_owned(), images[0].to_owned(), oper).await; 
+         
+
+        std::fs::remove_dir_all(Process.to_owned() + "/" + &ImageBucket + "/" + &PublicID.to_owned()).unwrap();
+
+        let result = json!({
+            "Result": "Success",
+            "Publicid": PublicID,
+            "Collection_Publicid": collection["Publicid"]
+        });
+
+        return result;
+    } else if poster == true {
+
+        FfmpegCommand::new()
+            .input(image_path)
+            .hide_banner()
+            .arg("-y")
+            .arg(Paths[0].as_str())
+            .spawn()
+            .unwrap()
+            .iter()
+            .expect("Image Not comverted");
+
+        let properties = json!({
+            "Poster": true,
+            "Album": false,
+            "Avatar": false
+        });
         let image = v_media::ActiveModel {
             id: ActiveValue::Set(ID),
             publicid: ActiveValue::Set(PublicID.to_owned()),
@@ -254,51 +306,6 @@ pub async fn ProcessImages(
         });
 
         return result;
-    } else if poster == true {
-
-        FfmpegCommand::new()
-            .input(image_path)
-            .hide_banner()
-            .arg("-y")
-            .arg(Paths[0].as_str())
-            .spawn()
-            .unwrap()
-            .wait()
-            .expect("Image Not converted");
-
-        let properties = json!({
-            "Poster": true,
-            "Album": false,
-            "Avatar": false
-        });
-        let image = v_media::ActiveModel {
-            id: ActiveValue::Set(ID),
-            publicid: ActiveValue::Set(PublicID.to_owned()),
-            title: ActiveValue::Set(name.to_owned()),
-            mediatype: ActiveValue::Set("Image".to_string()),
-            uploaded_at: ActiveValue::Set(DateTime::new(now.date_naive(), now.time())),
-            username: ActiveValue::Set(username),
-            description: ActiveValue::NotSet,
-            chapters: ActiveValue::NotSet,
-            storagepathorurl: ActiveValue::Set(Some(UploadPath.to_owned())),
-            poster_storagepathorurl: ActiveValue::NotSet,
-            properties: ActiveValue::Set(Some(properties)),
-            state: ActiveValue::Set(Media::MediaState::Published.to_string()),
-        };
-
-        let image: v_media::Model = image.insert(&connection).await.unwrap();
-
-        PushImage(Paths[0].to_owned(), PublicID.to_owned(), images[0].to_owned(), oper).await; 
-
-        std::fs::remove_dir_all(Process.to_owned() + "/" + &ImageBucket + "/" + &PublicID.to_owned()).unwrap();
-
-        let result = json!({
-            "Result": "Success",
-            "Publicid": PublicID,
-            "Poster": UploadPath
-        });
-
-        return result;
     }
 
     else if avatar == true {
@@ -311,7 +318,7 @@ pub async fn ProcessImages(
             .spawn()
             .unwrap()
             .wait()
-            .expect("Image Not converted");
+            .expect("Image Not comverted");
 
         let properties = json!({
             "Poster": false,
@@ -371,13 +378,15 @@ pub async fn ProcessImages(
             .spawn()
             .unwrap()
             .wait()
-            .expect("Image Not converted");
+            .expect("Image Not comverted");
 
         let properties = json!({
             "Poster": false,
             "Album": false,
             "Avatar": false
         });
+
+        let ImageStorageURL = ImageStorageUrl + "/" + &images[0];
 
         let image = v_media::ActiveModel {
             id: ActiveValue::Set(ID),
